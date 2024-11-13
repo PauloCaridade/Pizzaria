63c3f7d07b472d6db5991d3a088c9030ef9c57a3
Lucas Cunha da Silva <cdslucas07@gmail.com>
2024-11-12 22:01:10 -0300

    Cadastrar e Alterar os Produtos

--- src/cardapio/Adicional.java
+++ src/cardapio/Adicional.java
@@ -4,4 +4,9 @@ public class Adicional extends ItemCardapio {
     public Adicional(String nome, double preco) {
         super(nome, preco);  // Chama o construtor da classe ItemCardapio
     }
+
+    @Override
+    public String toString() {
+        return "Adicional: " + getNome() + ", Preço: R$ " + getPreco();
+    }
 }
--- src/cardapio/Pizza.java
+++ src/cardapio/Pizza.java
@@ -4,4 +4,9 @@ public class Pizza extends ItemCardapio {
     public Pizza(String nome, double preco) {
         super(nome, preco);  // Chama o construtor da classe ItemCardapio
     }
+
+    @Override
+    public String toString() {
+        return "Pizza: " + getNome() + ", Preço: R$ " + getPreco();
+    }
 }
